bd96ea181ed8a1e1f47a5fb89923961719237b1a
Greg Nowicki <nowicki.greg@gmail.com>
2014-04-29 19:45:36 -0400

    modified config/app.rb file

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+#config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
 module Radiusapp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -21,6 +23,6 @@ module Radiusapp
     # config.i18n.default_locale = :de
   end
 
-config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
 
 end
